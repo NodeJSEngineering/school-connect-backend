001b3e0365130585f33dc7e447900f54a4b8959c
Sunny Gupta <sunny.gupta@globallogic.com>
2022-01-09 22:15:35 +0530

    user schema update

--- DB.js
+++ DB.js
@@ -1,8 +1,11 @@
 module.exports = {
-    DB: 'mongodb://localhost:27017/schoolconnect',
+    // DB: 'mongodb://localhost:27017/schoolconnect',
     secret: 'test',
 
-    url: "mongodb://localhost:27017/",
+    // url: "mongodb://localhost:27017/",
+    DB: 'mongodb+srv://test:test123@cluster0.dietn.mongodb.net/schoolconnect?retryWrites=true&w=majority',
+    url: "mongodb+srv://test:test123@cluster0.dietn.mongodb.net/",
+
     database: "schoolconnect",
     imgBucket: "photos",
   };
